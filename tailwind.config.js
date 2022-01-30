dbc00841a4ca141a80f08e5a672069642278a03e
Lutimi <misael@wemake.pe>
2022-01-30 15:12:10 -0500

    Basic Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}" ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu","sans-serif"]
+
+      },
+      colors: {
+        'blackp': '#1C1C1C',
+        'pinky': '#EA4D6F',
+        'orange': '#E6696D',
+        'dark-green1': '#03707D',
+        'aqua': '#55B1A2',
+        'blanquito': '#E8E6E4',
+        
+      },
+
+    },
+  },
+  plugins: [],
+}
