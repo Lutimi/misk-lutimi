0368de5243e70230efab5e861cd4af0b6fd71da9
Lutimi <misael@wemake.pe>
2022-02-13 23:46:02 -0500

    Fix App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
 
-import miniImagen from "./assets/images/Character.png";
-import Imagen from "./assets/images/Logo.png";
-import Navbar from "./components/NavBar";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Footer from "./components/Footer";
-import ItemDetail from "./components/ItemDetail";
 function app() {
   return (
     <div className="App">
